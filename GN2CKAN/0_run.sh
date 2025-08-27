093c1dc2c05f486a0548ee8fb11551258c6fa29e
eloi=ribeiro <mail@eloiribeiro.eu>
2025-08-27 19:33:44 +0200

    .

--- GN2CKAN/0_run.sh
+++ GN2CKAN/0_run.sh
@@ -2,27 +2,23 @@
 
 
 # variables
+clear
 PROJECT_DIR="/home/carva014/Work/Code/FAO/GloSIS-private/GN2CKAN"
-
 
 # conda env
 eval "$(conda shell.bash hook)"
 conda activate db
-
 
 # copy xml's from bucket
 cd /home/carva014/Downloads
 ./google-cloud-sdk/bin/gcloud init
 gsutil -m cp gs://fao-maps-catalog-data/geonetwork_metadata/paper_maps/*.xml /home/carva014/Downloads/FAO/Metadata/
 
-
 # create db schema
 psql -h localhost -p 5432 -d iso19139 -U sis -f $PROJECT_DIR/1_schema.sql
 
-
 # xml to db
 python $PROJECT_DIR/2_xml2db.py
-
 
 # db to xml
 python $PROJECT_DIR/4_db2xml.py
